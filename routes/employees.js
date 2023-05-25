3d3003a406e7ce5f6964752345672cce7e6d036e
Marat <evolution99@rambler.ru>
2023-05-25 06:54:54 +0300

    Edited

--- routes/employees.js
+++ routes/employees.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const router = express.Router(); 
-const {all, add} = require('../controllers/employees')
+const {all, add, remove, employee, edit} = require('../controllers/employees')
 const {auth} = require('../middleware/auth');
 
 // /api/employees
 router.get('/', auth, all);
 // /api/employees/:id
-router.get('/:id', auth, () => console.log('get single employe'));
+router.get('/:id', auth, employee);
 // /api/employees/add
 router.post('/add', auth, add);
 // /api/employees/remove/:id
-router.post('/remove/:id', auth, () => console.log('remove employee'));
+router.post('/remove/:id', auth, remove);
 // /api/employees/edit/:id
-router.get('/edit/:id', auth, () => console.log('remove employee'));
+router.put('/edit/:id', auth, edit);
 
 module.exports = router;
