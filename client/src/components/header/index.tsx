7959af8bb4cc801ffb5617ccd328cd9268e368df
Marat <evolution99@rambler.ru>
2023-05-28 15:59:26 +0300

    Edited

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,13 +1,28 @@
-import { TeamOutlined } from '@ant-design/icons';
-import {Button, Layout, Space, Typography} from 'antd';
+import { LoginOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
+import { Layout, Space, Typography } from 'antd';
+import { Link } from 'react-router-dom';
 import styles from './index.module.css';
+import { CustomButton } from '../custom-button';
+import { Paths } from '../../paths';
 
 export const Header = () => {
   return (
     <Layout.Header className={styles.header}>
         <Space>
             <TeamOutlined className={styles.teamIcon}/>
-            <Button type='link'>Click</Button>
+            <Link to={Paths.home}>
+              <CustomButton type='ghost'>
+                <Typography.Title level={1}>Сотрудники</Typography.Title>  
+              </CustomButton>
+            </Link>
+        </Space>
+        <Space>
+          <Link to={Paths.register}>
+            <CustomButton type="ghost" icon={<UserOutlined />}>Зарегистрировать</CustomButton>
+          </Link>
+          <Link to={Paths.login}>
+            <CustomButton type="ghost" icon={<LoginOutlined />}>Войти</CustomButton>
+          </Link>
         </Space>
     </Layout.Header>
   )
