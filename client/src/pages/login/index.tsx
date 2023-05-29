1c445607411876fec6ab32d09a91f81e1a56d68b
Marat <evolution99@rambler.ru>
2023-05-29 07:36:00 +0300

    Edited

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,16 +1,30 @@
-import {Layout} from '../../components/layout'
-import { Card, Form, Input, Row } from 'antd'
+import { CustomInput } from "../../components/custom-input";
+import { Layout } from "../../components/layout";
+import { Card, Form, Row, Space, Typography } from "antd";
+import { PasswordInput } from "../../components/password-input/input";
+import { CustomButton } from "../../components/custom-button";
+import { Link } from "react-router-dom";
+import { Paths } from "../../paths";
 
 export const Login = () => {
   return (
     <Layout>
       <Row align="middle" justify="center">
-        <Card title="Войдите" style={{ width: "30rem"}}>
+        <Card title="Войдите" style={{ width: "30rem" }}>
           <Form onFinish={() => null}>
-            <Input />
-          </Form>          
+            <CustomInput type="email" name="email" placeholder="Email" />
+            <PasswordInput name="password" placeholder="Пароль" />
+            <CustomButton type="primary" htmlType="submit">
+              Войти
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Нет аккаунта? <Link to={Paths.register}>Зарегистриуйтесь</Link>
+            </Typography.Text>
+          </Space>
         </Card>
       </Row>
     </Layout>
-  )
-}
+  );
+};
