5f5f78ae8b32f5109c55986b706a24b962b77490
Marat <evolution99@rambler.ru>
2023-05-31 07:38:56 +0300

    Edited edited.tsx

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { CustomInput } from "../../components/custom-input";
 import { Layout } from "../../components/layout";
 import { Card, Form, Row, Space, Typography } from "antd";
@@ -5,13 +6,32 @@ import { PasswordInput } from "../../components/password-input/input";
 import { CustomButton } from "../../components/custom-button";
 import { Link } from "react-router-dom";
 import { Paths } from "../../paths";
+import { UserData, useLoginMutation } from "../../app/Services/auth";
+import { isErrorWithMessage } from "../is-error-with-message";
 
 export const Login = () => {
+  const [loginUser, loginUserResult] = useLoginMutation()
+  const [ error, setError ] = useState('');
+
+  const login = async (data: UserData) => {
+    try {
+      await loginUser(data).unwrap();
+
+    } catch (err) {
+      const maybeError = isErrorWithMessage(err);
+
+      if (maybeError) {
+
+      }
+    }
+
+  }
+
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Войдите" style={{ width: "30rem" }}>
-          <Form onFinish={() => null}>
+          <Form onFinish={ login }>
             <CustomInput type="email" name="email" placeholder="Email" />
             <PasswordInput name="password" placeholder="Пароль" />
             <CustomButton type="primary" htmlType="submit">
