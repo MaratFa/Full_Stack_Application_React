b523f269bd57ec47ebe46cb369d951aa0f6eac83
Marat <evolution99@rambler.ru>
2023-06-02 23:10:04 +0300

    Edited api.ts in Services folder

--- client/src/app/Services/api.ts
+++ client/src/app/Services/api.ts
@@ -1,10 +1,14 @@
 import { createApi, fetchBaseQuery, retry } from '@reduxjs/toolkit/query/react'
+import { RootState } from '../store';
 
 const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:8000/api',
+    prepareHeaders(headers, { getState }) {
+        const token = (getState() as RootState).auth.user?.token;
+    },
 });
 
-const baseQueryWithRetry = retry(baseQuery, { maxRetries: 1});
+const baseQueryWithRetry = retry(baseQuery, { maxRetries: 1 });
 
 export const api = createApi({
     reducerPath: 'splitApi',
