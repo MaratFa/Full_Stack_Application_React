b2d5f6f77570c3b90cf9b44cac853ed4994ba669
Marat <evolution99@rambler.ru>
2023-06-03 21:28:43 +0300

    Edited index.tsx in src folder

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,41 +1,45 @@
-import React from 'react';
-import { ConfigProvider, theme } from 'antd';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { createBrowserRouter,RouterProvider } from 'react-router-dom';
-import { store } from './app/store';
-import reportWebVitals from './reportWebVitals';
-import { Paths } from './paths';
-import { Login } from './pages/login';
-import { Register } from './pages/register';
-import './index.css';
+import React from "react";
+import { ConfigProvider, theme } from "antd";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { store } from "./app/store";
+import reportWebVitals from "./reportWebVitals";
+import { Paths } from "./paths";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import "./index.css";
+import { Auth } from "./features/auth/auth";
 
-
-const router = createBrowserRouter ([
+const router = createBrowserRouter([
   {
     path: Paths.home,
-    element: <h1>Employees</h1>
+    element: <h1>Employees</h1>,
   },
   {
     path: Paths.login,
-    element: <Login />
+    element: <Login />,
   },
   {
     path: Paths.register,
-    element: <Register /> 
+    element: <Register />,
   },
-])
+]);
 
-const container = document.getElementById('root')!;
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ConfigProvider theme={{
-        algorithm: theme.darkAlgorithm
-      }}>
-        <RouterProvider router={router} />
+      <ConfigProvider
+        theme={{
+          algorithm: theme.darkAlgorithm,
+        }}
+      >
+        <Auth>
+          <RouterProvider router={router} />
+        </Auth>
       </ConfigProvider>
     </Provider>
   </React.StrictMode>
