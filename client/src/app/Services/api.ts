2fce73f8b065e968df6fcf4112009f5866ebc6ba
Marat <evolution99@rambler.ru>
2023-06-03 21:30:03 +0300

    Edited api.ts in Services folder

--- client/src/app/Services/api.ts
+++ client/src/app/Services/api.ts
@@ -4,8 +4,12 @@ import { RootState } from '../store';
 const baseQuery = fetchBaseQuery({
     baseUrl: 'http://localhost:8000/api',
     prepareHeaders(headers, { getState }) {
-        const token = (getState() as RootState).auth.user?.token;
-    },
+        const token = (getState() as RootState).auth.user?.token || localStorage.getItem('token');
+
+        if (token && token !== null) {
+            headers.set('Authorization', `Bearer ${token}`);
+        }
+    }
 });
 
 const baseQueryWithRetry = retry(baseQuery, { maxRetries: 1 });
